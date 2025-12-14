04df98d0fca0d209ca506e4a8e73b538355cdff0
SashaBerkowsky <snberkowsky@gmail.com>
2025-12-13 22:43:15 -0300

    add wasm

---
+++ static/wasm/wasm_game_of_life_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_universe_free: (a: number, b: number) => void;
+export const greet: (a: number, b: number) => void;
+export const universe_cells: (a: number) => number;
+export const universe_height: (a: number) => number;
+export const universe_new: (a: number, b: number) => number;
+export const universe_nuke: (a: number) => void;
+export const universe_render: (a: number) => [number, number];
+export const universe_reset: (a: number) => void;
+export const universe_set_height: (a: number, b: number) => void;
+export const universe_set_width: (a: number, b: number) => void;
+export const universe_spawn_glider: (a: number, b: number, c: number) => void;
+export const universe_spawn_pulsar: (a: number, b: number, c: number) => void;
+export const universe_tick: (a: number) => void;
+export const universe_toggle_cell: (a: number, b: number, c: number) => void;
+export const universe_width: (a: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
